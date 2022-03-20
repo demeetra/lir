77cad0648da49cf2f42c8cd610273e6b3e9f8d68
demeetra <71707122+demeetra@users.noreply.github.com>
2022-03-20 21:45:58 +0300

    images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_20_161630) do
+ActiveRecord::Schema.define(version: 2022_03_20_183707) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2022_03_20_161630) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
+    t.string "image"
   end
 
   create_table "subscribers", force: :cascade do |t|
