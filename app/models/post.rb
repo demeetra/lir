3679bd3b1168f36b24510060fe0dc67096130fcb
demeetra <71707122+demeetra@users.noreply.github.com>
2022-03-20 18:34:57 +0300

    comments delete

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
 		validates :title, :presence => true,
 											:length => { :minimum => 5 }
 
-		has_many :comments
+		has_many :comments, :dependent => :destroy
 end
