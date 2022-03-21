956e67241bd291b8ddac82f84ff497714bccb036
demeetra <71707122+demeetra@users.noreply.github.com>
2022-03-21 17:14:14 +0300

    sign up\sign in and access rights

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_20_183707) do
+ActiveRecord::Schema.define(version: 2022_03_21_140707) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -18,7 +18,9 @@ ActiveRecord::Schema.define(version: 2022_03_20_183707) do
     t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -31,6 +33,8 @@ ActiveRecord::Schema.define(version: 2022_03_20_183707) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
     t.string "image"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "subscribers", force: :cascade do |t|
@@ -39,5 +43,19 @@ ActiveRecord::Schema.define(version: 2022_03_20_183707) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,25 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Post.destroy_all
 puts "Destroyed everything you touch"
-
-posts = [
-{
-	name: "New Album",
-	title: "The Bad Seeds",
-	author: "Nick Cave",
-	content: "Tracklist",
-	image: URI.open("https://in-sound.ru/upload/iblock/d85/d859aa8387fd5fcd7a933be2af91066b.jpg")
-},
-{
-	name: "Old Album",
-	title: "The Bad Seeds Old",
-	author: "Nick Cave Old Too",
-	content: "Some Old Tracklist",
-	image: URI.open("https://in-sound.ru/upload/iblock/d85/d859aa8387fd5fcd7a933be2af91066b.jpg")
-}
-]
-
-posts.each do |post|
-	postik = Post.create(post)
-	puts "Some magic just create a #{ postik.name } with id #{ postik.id }!"
-end
+#
+# posts = [
+# {
+# 	name: "New Album",
+# 	title: "The Bad Seeds",
+# 	author: "Nick Cave",
+# 	content: "Tracklist",
+# 	image: URI.open("https://in-sound.ru/upload/iblock/d85/d859aa8387fd5fcd7a933be2af91066b.jpg")
+# },
+# {
+# 	name: "Old Album",
+# 	title: "The Bad Seeds Old",
+# 	author: "Nick Cave Old Too",
+# 	content: "Some Old Tracklist",
+# 	image: URI.open("https://in-sound.ru/upload/iblock/d85/d859aa8387fd5fcd7a933be2af91066b.jpg")
+# }
+# ]
+#
+# posts.each do |post|
+# 	postik = Post.create(post)
+# 	puts "Some magic just create a #{ postik.name } with id #{ postik.id }!"
+# end
