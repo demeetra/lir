3f147bea83effd9ceeb36a02db242731204e2a4b
demeetra <71707122+demeetra@users.noreply.github.com>
2022-03-22 19:47:11 +0300

    remove name from post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_21_204115) do
+ActiveRecord::Schema.define(version: 2022_03_22_164515) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(version: 2022_03_21_204115) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "name"
     t.string "title"
     t.text "content"
     t.date "date"
@@ -71,7 +70,6 @@ ActiveRecord::Schema.define(version: 2022_03_21_204115) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -79,6 +77,7 @@ ActiveRecord::Schema.define(version: 2022_03_21_204115) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.boolean "isadmin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
