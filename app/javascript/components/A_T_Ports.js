eb5d1cb2a65e88766ec84bdcaf7f90260637cc7f
demeetra <71707122+demeetra@users.noreply.github.com>
2022-06-26 20:09:51 +0300

    all

--- app/javascript/components/A_T_Ports.js
+++ app/javascript/components/A_T_Ports.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 class ATPorts extends React.Component {
   render () {
     return (
-      <button className="top_menu_button" onClick={event =>  window.location.href=this.props.href}><h2>Порты</h2></button>
+      <button className="top_menu_button" onClick={event =>  window.location.href=this.props.href}><h2>Карта</h2></button>
     );
   }
 }
